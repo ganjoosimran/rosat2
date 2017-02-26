70280f6f887b1ef28724cf3084f489a65954fcaa
Simran Ganjoo <ganjoosimran070@gmail.com>
2017-02-26 18:40:48 +0530

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,51 @@
+function findselectedg() {
+    var result = document.querySelector('input[name="childreng"]:checked').value;
+    if(result=="Yes"){
+
+        document.getElementById("inputtextg").setAttribute('disabled', true);
+    }
+    else{
+        document.getElementById("inputtextg").removeAttribute('disabled');
+    }
+}
+function findselecteda() {
+    var result = document.querySelector('input[name="childrena"]:checked').value;
+    if(result=="Yes"){
+
+        document.getElementById("inputtexta").setAttribute('disabled', true);
+    }
+    else{
+        document.getElementById("inputtexta").removeAttribute('disabled');
+    }
+}
+function findselectedb() {
+    var result = document.querySelector('input[name="childrenb"]:checked').value;
+    if(result=="Yes"){
+
+        document.getElementById("inputtextb").setAttribute('disabled', true);
+    }
+    else{
+        document.getElementById("inputtextb").removeAttribute('disabled');
+    }
+}
+function findselectedc() {
+    var result = document.querySelector('input[name="childrenc"]:checked').value;
+    if(result=="Yes"){
+
+        document.getElementById("inputtextc").setAttribute('disabled', true);
+    }
+    else{
+        document.getElementById("inputtextc").removeAttribute('disabled');
+    }
+}
+
+function findselected() {
+    var result = document.querySelector('input[name="children"]:checked').value;
+    if(result=="Yes"){
+
+        document.getElementById("inputtext").setAttribute('disabled', true);
+    }
+    else{
+        document.getElementById("inputtext").removeAttribute('disabled');
+    }
+}
