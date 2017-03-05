aa632ea383542d71043e81ad8d9fc802edf9ffaa
Utkarsh <utkarshbhimte95@gmail.com>
2017-03-05 18:41:31 +0530

    added scrolltop to sliding animaioj

--- js/script1.js
+++ js/script1.js
@@ -48,3 +48,11 @@ function findselected() {
         $("#inputtext").removeAttribute('disabled');
     }
 }
+$( document ).ready(function () {
+
+    $('#myCarousel').bind('slide.bs.carousel', function (e) {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        console.log('slide event!');
+    });
+
+});
