931995500bd100506356523be1b839eeb02041c6
Simran Ganjoo <ganjoosimran070@gmail.com>
2017-03-14 06:14:43 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 $(".progress-bar").css('width', '0%');
 <script>
   $("#header").load("header.html");
   $("#footer").load("footer.html");
 </script>
-=======
-$(".progress-bar").css('width', '0%');
-<script>
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
-</script>
->>>>>>> 4467263849be91909097b608250755f4cf2e8e68
